2ed2baf8b7b30271ae04ef23ad6d5c37dd3ddf2f
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-16 22:38:27 +0200

    Inizializzaizone del progetto con la creazione di header,mai e footer, creazione della cartella SCSS, esportazione di bootstrap e del font

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="">
+    <div class="row container-sm text-center">
+      <div class="col">
+        <img src="../assets/avada-charity-sticky-header-logo.png" alt="" />
+      </div>
+      <div class="col">
+        <ul >
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+data(){
+    
+}
+};
+</script>
+
+<style lang="scss" scoped></style>

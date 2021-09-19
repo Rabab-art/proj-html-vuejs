68619479441936697fae2c80d0c5d2ffc16a236c
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-19 22:18:45 +0200

    Terza sezione del main che comprende una serie di immagini

---
+++ src/components/ArticlesSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <div class="article-bar text-center d-flex">
+      <p class="p-5">VIE ALL OUR CAUSES</p>
+    </div>
+    <div class="row"></div>
+  </section>
+</template>
+
+<script>
+export default {
+    name:"ArticleSection",
+};
+</script>
+
+<style lang="scss" scoped></style>
